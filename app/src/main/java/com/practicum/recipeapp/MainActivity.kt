a41cc02cb9bcb1a9d88e8a0d616b4b4d3152d74e
4meech <hornax@gmail.com>
2024-08-17 16:19:39 +0300

    RA-04-1: Fragment created

--- app/src/main/java/com/practicum/recipeapp/MainActivity.kt
+++ app/src/main/java/com/practicum/recipeapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.practicum.recipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.practicum.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,18 @@ class MainActivity : AppCompatActivity() {
     private val binding
         get() = _binding ?: throw IllegalStateException("Main Activity binding can not be null")
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            val fragmentManager = supportFragmentManager
+            val categoriesListFragment = CategoriesListFragment()
+            fragmentManager.commit {
+                add(R.id.mainContainer, categoriesListFragment)
+            }
+        }
     }
 }
