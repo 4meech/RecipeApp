e0ae768551db97250b1822219af926a34a0a1e41
4meech <hornax@gmail.com>
2024-08-19 15:28:51 +0300

    RA-04-2: code style improvements

--- app/src/main/java/com/practicum/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/practicum/recipeapp/CategoriesListFragment.kt
@@ -7,17 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.practicum.recipeapp.databinding.FragmentListCategoriesBinding
 
-class CategoriesListFragment : Fragment(R.layout.fragment_list_categories){
+class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
     private var _binding: FragmentListCategoriesBinding? = null
     private val binding
         get() = _binding ?: throw IllegalStateException("CatList Fragment binding can not be null")
 
     override fun onCreateView(
-        i: LayoutInflater,
-        c: ViewGroup?,
-        s: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentListCategoriesBinding.inflate(i, c, false)
+        _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
